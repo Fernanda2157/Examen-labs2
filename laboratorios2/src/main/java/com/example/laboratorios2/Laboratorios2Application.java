43792acc32d722330b9dfa394e6695d51da548c1
Maria Fernanda <mfllanos@uce.edu.ec>
2026-08-04 12:17:12 -0500

    first commit

---
+++ laboratorios2/src/main/java/com/example/laboratorios2/Laboratorios2Application.java
@@ -0,0 +1,13 @@
+package com.example.laboratorios2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorios2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorios2Application.class, args);
+	}
+
+}
---
+++ laboratorios2/src/test/java/com/example/laboratorios2/Laboratorios2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laboratorios2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorios2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
